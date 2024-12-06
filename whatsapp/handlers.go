311304ea4b927ae98d64c8c953edb14e799ec309
Igor Graziano <igorrsgraziano@gmail.com>
2024-12-06 02:21:51 -0300

    added response handler

--- whatsapp/handlers.go
+++ whatsapp/handlers.go
@@ -1,18 +1,60 @@
 package whatsapp
 
 import (
+	"context"
 	"fmt"
 
+	"math/rand"
+
+	"go.mau.fi/whatsmeow"
+	"go.mau.fi/whatsmeow/proto/waE2E"
 	"go.mau.fi/whatsmeow/types/events"
 )
 
-func eventHandler(evt interface{}) {
-	switch v := evt.(type) {
-	case *events.Message:
-		fmt.Println("Received a message!", v.Message.GetConversation())
+func eventHandler(client *whatsmeow.Client) func(interface{}) {
+	return func(evt interface{}) {
+		switch v := evt.(type) {
+		case *events.Message:
+			handleMessage(client, v)
+		default:
+			fmt.Println("Received a message!", v)
+		}
+
 	}
 }
 
-func handleMessage(msg *events.Message) {
-	fmt.Println("Received a message!", msg.Message.GetConversation())
+func isFromEnabledGroup(msg *events.Message) bool {
+	//TODO: Colocar em um env
+	return msg.Info.MessageSource.Chat.User == "120363346101975622"
+}
+
+func isActivateCommand(msg *events.Message) bool {
+	return msg.Message.GetConversation() == "!nathan"
+}
+
+func handleMessage(client *whatsmeow.Client, msg *events.Message) {
+	if !isFromEnabledGroup(msg) || !isActivateCommand(msg) {
+		return
+	}
+
+	quotes := []string{
+		"Frase 1",
+		"Frase 2",
+		"Frase 3",
+	}
+
+	randomQuote := &quotes[rand.Intn(len(quotes))]
+
+	ctx := context.Background()
+
+	//TODO: Colocar quotedMessage
+	r, err := client.SendMessage(ctx, msg.Info.Chat, &waE2E.Message{
+		Conversation: randomQuote,
+	})
+
+	fmt.Println(r)
+
+	if err != nil {
+		client.Log.Errorf("Error sending message: %v", err)
+	}
 }
--- whatsapp/whatsapp.go
+++ whatsapp/whatsapp.go
@@ -60,7 +60,7 @@ func Start() {
 	client := startClient(container)
 	login(client)
 
-	client.AddEventHandler(eventHandler)
+	client.AddEventHandler(eventHandler(client))
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
